4c59ea20f135d9297d5e93d2e505567fd4843742
Francesco Del Giudice <francesco.delgiudice@heply.it>
2021-06-28 12:36:50 +0200

    fix: default info

--- src/pages/products/ProductsCategory.vue
+++ src/pages/products/ProductsCategory.vue
@@ -55,95 +55,12 @@ export default {
   data () {
     return {
       category: {
-        parent_id: '11',
-        parent_name: 'Acetabular Cups',
-        list: [
-          {
-            id: 144,
-            name: 'DELTA TT',
-            subcategory: 12,
-            subtitle: 'DELTA system Primary',
-            preview:
-              'https://limacorporate.com/repo/product-preview/7320828c9153b2a9848d6bc45d3544236b22fc48/o_1ac4obths1rdt168muk71q181r4rib_tmb.jpg'
-          },
-          {
-            id: 147,
-            name: 'DELTA ST-C',
-            subcategory: 12,
-            subtitle: 'DELTA system Primary',
-            preview:
-              'https://limacorporate.com/repo/product-preview/b3c0730cf3f50613e40561e67c871fdb92820cf9/o_1aa762jp673t17t2cf01mha13hi1e5_tmb.jpg'
-          },
-          {
-            id: 145,
-            name: 'DELTA PF',
-            subcategory: 12,
-            subtitle: 'DELTA system Primary',
-            preview:
-              'https://limacorporate.com/repo/product-preview/50336bc687eb161ee9fb0ddb8cf2b7e65bad865f/o_1aa75t7srvh21vna1rdc1d3nlk41c4_tmb.jpg'
-          },
-          {
-            id: 146,
-            name: 'DELTA Fins',
-            subcategory: 12,
-            subtitle: 'DELTA system Primary',
-            preview:
-              'https://limacorporate.com/repo/product-preview/3fcfb99ec010d4a8ba364f43169465d91ca39ada/o_1aae19p253i6iu5bhjcit1h8p49_tmb.jpg'
-          }
-        ],
-        filters: {
-          subcategories: [
-            {
-              id: '',
-              label: 'All'
-            },
-            {
-              id: 12,
-              label: 'DELTA system Primary'
-            },
-            {
-              id: 13,
-              label: 'DELTA system Revision'
-            }
-          ],
-          order: [
-            {
-              id: 'nameASC',
-              label: 'Name (A-Z)'
-            },
-            {
-              id: 'nameDESC',
-              label: 'Name (Z-A)'
-            }
-          ]
-        }
+        parent_id: '0',
+        parent_name: 'Category',
+        list: [],
+        filters: {}
       },
-      filtersOptions: {
-        subcategories: [
-          {
-            id: '',
-            label: 'All'
-          },
-          {
-            id: 12,
-            label: 'DELTA system Primary'
-          },
-          {
-            id: 13,
-            label: 'DELTA system Revision'
-          }
-        ],
-        order: [
-          {
-            id: 'nameASC',
-            label: 'Name (A-Z)'
-          },
-          {
-            id: 'nameDESC',
-            label: 'Name (Z-A)'
-          }
-        ]
-      },
+      filtersOptions: {},
       filters: {
         order: '',
         subcategory: ''
