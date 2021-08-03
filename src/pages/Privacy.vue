ade2186079ee645f335abe9b024178732c6bfe70
Francesco Del Giudice <francesco.delgiudice@heply.it>
2021-08-03 15:54:46 +0200

    chore: add link to full privacy policy

--- src/pages/Privacy.vue
+++ src/pages/Privacy.vue
@@ -4,6 +4,14 @@
       class="px-8 font-helvetica text-mid-dark-grey text-16 spacing-1 line-24 mb-4"
       v-html="formattedPrivacy"
     />
+
+    <a
+      class="font-helvetica text-mid-dark-grey text-12 spacing-1 line-24 mt-4 z-10"
+      href="https://limacorporate.com/en/about-us/privacy-policy.html"
+      title="LimaCorporate Privacy Policy"
+      target="_blank"
+      >Read the full Privacy Policy</a
+    >
   </Page>
 </template>
 
