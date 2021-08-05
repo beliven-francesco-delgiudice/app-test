0565b070d636dec29cd79f7861dc36acc65b4b8c
Francesco Del Giudice <francesco.delgiudice@heply.it>
2021-08-05 10:21:14 +0200

    feat: added drag scroll

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,21 +1,58 @@
 <template>
-  <div
-    :class="[
-      height || '',
-      classes || '',
-      'flex w-full max-w-full overflow-x-auto carousel overflow-y-visible'
-    ]"
-  >
-    <slot></slot>
+  <div class="relative">
+    <ion-scroll
+      :scrollX="true"
+      :class="[
+        height || '',
+        classes || '',
+        'flex w-full max-w-full overflow-x-auto carousel overflow-y-visible carousel'
+      ]"
+    >
+      <slot></slot>
+    </ion-scroll>
   </div>
 </template>
 
 <script>
+import { IonScroll } from '@ionic/vue'
 export default {
-  components: {},
+  components: {
+    IonScroll
+  },
   props: {
     height: [Number, String],
     classes: [String, Array]
+  },
+  methods: {
+    scrollLeft () {}
+  },
+  mounted () {
+    const slider = document.querySelector('.carousel')
+    let isDown = false
+    let startX
+    let scrollLeft
+
+    slider.addEventListener('mousedown', e => {
+      isDown = true
+      slider.classList.add('active')
+      startX = e.pageX - slider.offsetLeft
+      scrollLeft = slider.scrollLeft
+    })
+    slider.addEventListener('mouseleave', () => {
+      isDown = false
+      slider.classList.remove('active')
+    })
+    slider.addEventListener('mouseup', () => {
+      isDown = false
+      slider.classList.remove('active')
+    })
+    slider.addEventListener('mousemove', e => {
+      if (!isDown) return
+      e.preventDefault()
+      const x = e.pageX - slider.offsetLeft
+      const walk = (x - startX) * 3 //scroll-fast
+      slider.scrollLeft = scrollLeft - walk
+    })
   }
 }
 </script>
