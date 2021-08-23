3cdac7825d5b81e727538382ea9b409d316c1852
Francesco Del Giudice <francesco.delgiudice@heply.it>
2021-08-23 12:23:03 +0200

    wip: slider tryfix

--- src/components/Slider.vue
+++ src/components/Slider.vue
@@ -14,46 +14,52 @@
     </ion-button>
 
     <ion-slides
+      mode="ios"
       ref="slides"
-      @ionSlideDidChange="updateActiveIndex"
-      :pager="true"
+      pager="true"
+      style="position:absolute; top:0; left:0; width:100%; z-index:0"
+      :options="slideOpts"
+      @ionSlideNextEnd="onNextEnd"
+      @ionSlidePrevEnd="onPrevEnd"
     >
       <ion-slide v-for="(slide, i) in computedSlides" :key="i">
         <slot :item="slide"></slot>
       </ion-slide>
     </ion-slides>
-    <div v-if="back" class="flex justify-between  items-center px-8">
-      <ion-button
-        type="button"
-        class="mr-4"
-        style="--background:transparent;"
-        v-if="activeSlide > 0"
-        @click="previousSlide"
-      >
-        <ion-img
-          src="/assets/button-icons/back.svg"
-          class="pointer-events-none"
+    <div class="px-8" style="margin-top:150%;">
+      <div v-if="back" class="flex justify-between  items-center px-8">
+        <ion-button
+          type="button"
+          class="mr-4"
+          style="--background:transparent;"
+          v-if="activeSlide > 0"
+          @click="previousSlide"
+        >
+          <ion-img
+            src="/assets/button-icons/back.svg"
+            class="pointer-events-none"
+          />
+        </ion-button>
+        <big-button
+          :label="buttonLabel"
+          :bgClass="isLastSlide ? 'bg-black' : 'bg-light-grey'"
+          @onClick="buttonClick"
         />
-      </ion-button>
-      <big-button
-        :label="buttonLabel"
-        :bgClass="isLastSlide ? 'bg-black' : 'bg-light-grey'"
-        @onClick="buttonClick"
-      />
-      <ion-button
-        type="button"
-        class="pointer-events-none opacity-0 ml-4"
-        style="--background:transparent;"
-        v-if="activeSlide > 0"
-        @click="previousSlide"
-      >
-        <ion-img
-          src="/assets/button-icons/back.svg"
-          class="pointer-events-none"
-        />
-      </ion-button>
+        <ion-button
+          type="button"
+          class="pointer-events-none opacity-0 ml-4"
+          style="--background:transparent;"
+          v-if="activeSlide > 0"
+          @click="previousSlide"
+        >
+          <ion-img
+            src="/assets/button-icons/back.svg"
+            class="pointer-events-none"
+          />
+        </ion-button>
+      </div>
+      <big-button v-else :label="buttonLabel" @onClick="buttonClick" />
     </div>
-    <big-button v-else :label="buttonLabel" @onClick="buttonClick" />
   </div>
 </template>
 <script>
@@ -75,8 +81,17 @@ export default {
   },
   data () {
     return {
-      active: 0
+      active: 0,
+      slideOpts: {
+        initialSlide: 1,
+        speed: 400
+      }
     }
+  },
+  mounted () {
+    setTimeout(() => {
+      window.location.reload
+    }, 500)
   },
   computed: {
     computedSlides () {
@@ -112,8 +127,11 @@ export default {
         this.$refs.slides.slideNext()
       }
     },
-    updateActiveIndex () {
-      this.active = this.$refs.slides.getActiveIndex()
+    onNextEnd () {
+      this.active = this.active + 1
+    },
+    onPrevEnd () {
+      this.active = this.active - 1
     }
   }
 }
