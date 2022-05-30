f1a3b3c9d3c37ac6f549f4e86000e5290be6a973
Francesco Del Giudice <francesco.delgiudice@heply.it>
2022-05-30 09:28:55 +0200

    fix: add check to avoid errors

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -17,21 +17,26 @@
 
 <script>
 import { IonScroll } from '@ionic/vue'
+
 export default {
   components: { IonScroll },
+
   props: {
     height: [Number, String],
     classes: [String, Array],
     id: String
   },
+
   methods: {
     scrollLeft () {}
   },
+
   computed: {
     computedID () {
       return this.id || 'carousel-' + Math.floor(Math.random() * 5)
     }
   },
+
   mounted () {
     let slider = null
     if (this.computedID) {
@@ -44,27 +49,29 @@ export default {
     let startX
     let scrollLeft
 
-    slider.addEventListener('mousedown', e => {
-      isDown = true
-      slider.classList.add('active')
-      startX = e.pageX - slider.offsetLeft
-      scrollLeft = slider.scrollLeft
-    })
-    slider.addEventListener('mouseleave', () => {
-      isDown = false
-      slider.classList.remove('active')
-    })
-    slider.addEventListener('mouseup', () => {
-      isDown = false
-      slider.classList.remove('active')
-    })
-    slider.addEventListener('mousemove', e => {
-      if (!isDown) return
-      e.preventDefault()
-      const x = e.pageX - slider.offsetLeft
-      const walk = (x - startX) * 3 //scroll-fast
-      slider.scrollLeft = scrollLeft - walk
-    })
+    if (slider) {
+      slider.addEventListener('mousedown', e => {
+        isDown = true
+        slider.classList.add('active')
+        startX = e.pageX - slider.offsetLeft
+        scrollLeft = slider.scrollLeft
+      })
+      slider.addEventListener('mouseleave', () => {
+        isDown = false
+        slider.classList.remove('active')
+      })
+      slider.addEventListener('mouseup', () => {
+        isDown = false
+        slider.classList.remove('active')
+      })
+      slider.addEventListener('mousemove', e => {
+        if (!isDown) return
+        e.preventDefault()
+        const x = e.pageX - slider.offsetLeft
+        const walk = (x - startX) * 3 //scroll-fast
+        slider.scrollLeft = scrollLeft - walk
+      })
+    }
   }
 }
 </script>
